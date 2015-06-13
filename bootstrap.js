2a87d08570cd9040cc14ca81932b71c2a42251fb
Asmageddon <Asmageddon@gmail.com>
2015-06-13 16:25:38 +0200

    Fixed not referencing the string bundle(s?)

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,8 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
 
+var stringBundle = Services.strings.createBundle('chrome://l10n/locale/global.properties?' + Math.random()); // Randomize URI to work around bug 719376
+
 function install() {}
 function uninstall() {}
 
